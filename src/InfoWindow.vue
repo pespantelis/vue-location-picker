8a1f9a568344ec8446aad19dee6c14e19105d433
Pantelis Peslis <pespantelis@gmail.com>
2016-04-18 00:47:26 +0300

    Show the address on dragging

---
+++ src/InfoWindow.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <strong>{{title}}</strong>
+    <p>{{body}}</p>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Instructions',
+        body: 'Click and drag this red marker to pick your location.'
+      }
+    },
+
+    methods: {
+      showAddress (place) {
+        this.title = 'My address'
+        this.body = place.formatted_address
+      },
+
+      showError () {
+        this.title = 'Oups'
+        this.body = 'Google Maps could not determine the approximate postal address of this location.'
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  div {
+    font-family: monospace;
+    color: #333;
+  }
+
+  p {
+    margin-bottom: 0;
+  }
+</style>
--- src/LocationPicker.vue
+++ src/LocationPicker.vue
@@ -1,24 +1,31 @@
 <template>
-  <div></div>
+  <div class="LocationPicker">
+    <div class="LocationPicker__map" v-el:map></div>
+    <info-window class="LocationPicker__info-window" v-ref:info></info-window>
+  </div>
 </template>
 
 
 <script>
+  import InfoWindow from './InfoWindow.vue'
+
   export default {
     data () {
       return {
+        geocoder: null,
         map: null,
-        marker: null
+        marker: null,
+        infoWindow: null
       }
     },
 
-    ready () {
-      this.$el.style.height = '100%'
-    },
+    components: { InfoWindow },
 
     events: {
       'location-picker-init' () {
-        this.map = new google.maps.Map(this.$el, {
+        this.geocoder = new google.maps.Geocoder()
+
+        this.map = new google.maps.Map(this.$els.map, {
           center: {lat: 39.3622, lng: 22.9422},
           zoom: 8,
           disableDefaultUI: true
@@ -26,9 +33,56 @@
 
         this.marker = new google.maps.Marker({
           map: this.map,
-          position: this.map.getCenter()
+          position: this.map.getCenter(),
+          draggable: true
+        })
+
+        this.infoWindow = new google.maps.InfoWindow({
+          content: this.$refs.info.$el,
+          maxWidth: 260
+        })
+
+        // events
+        google.maps.event.addListenerOnce(this.map, 'idle', this.openInfoWindow)
+        this.marker.addListener('dragstart', this.closeInfoWindow)
+        this.marker.addListener('dragend', this.geocodeLocation)
+      }
+    },
+
+    methods: {
+      openInfoWindow () {
+        this.infoWindow.open(this.map, this.marker)
+      },
+
+      closeInfoWindow () {
+        this.infoWindow.close()
+      },
+
+      geocodeLocation (e) {
+        this.map.panTo(e.latLng)
+
+        this.geocoder.geocode({'latLng': e.latLng}, (response) => {
+          if (response && response.length > 0) {
+            this.$refs.info.showAddress(response[0])
+          } else {
+            this.$refs.info.showError()
+          }
+
+          this.openInfoWindow()
         })
       }
     }
   }
 </script>
+
+
+<style>
+  .LocationPicker,
+  .LocationPicker__map {
+    height: 100%;
+  }
+
+  .LocationPicker > .LocationPicker__info-window {
+    display: none;
+  }
+</style>
