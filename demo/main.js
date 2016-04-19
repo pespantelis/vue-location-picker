1889e89e2b10af2384dd0119487c4224194463fa
Pantelis Peslis <pespantelis@gmail.com>
2016-04-19 09:58:47 +0300

    Create the demo

--- demo/main.js
+++ demo/main.js
@@ -9,7 +9,7 @@ new Vue({
     LocationPickerInit(this, { key: 'AIzaSyBmBM_Utpx3unBuhiTTR5yMptdMZSxelo4' }, {
       map: {
         center: { lat: 39.3622, lng: 22.9422 },
-        zoom: 8,
+        zoom: 7,
         minZoom: 3
       },
       marker: { icon: 'demo/assets/marker.png' },
