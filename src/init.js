153d3ff15f29241a78a6244410e6edd53f01bab0
Julien Cappiello <contact@kendozone.com>
2017-10-21 12:02:03 +0200

    trying to upgrade to VueJS 2

--- src/init.js
+++ src/init.js
@@ -1,34 +1,35 @@
 import Vue from 'vue'
 import LocationPicker from './LocationPicker.vue'
 
-export function install (app, options) {
-  if (! options.config || !options.config.key) {
-    console.error('[Vue Location Picker warn]: You should give a Google Maps API key')
-    return
-  }
+LocationPicker.install = function (Vue, options) {
+    console.log(options);
+    if (! options || !options.key) {
+        console.error('[Vue Location Picker warn]: You should give a Google Maps API key')
+        return
+    }
 
-  options.libraries = 'places'
-  options.callback = 'initLocationPicker'
+    options.libraries = 'places';
+    options.callback = 'initLocationPicker';
 
-  // set the global event bus
-  global.vueLocationPickerEventBus = new Vue()
+    // set the global event bus
+    global.vueLocationPickerEventBus = new Vue();
 
-  // set the callback function
-  global.initLocationPicker = () => {
-    global.vueLocationPickerEventBus.$emit('location-picker-init')
-  }
+    // set the callback function
+    global.initLocationPicker = () => {
+        global.vueLocationPickerEventBus.$emit('location-picker-init')
+    };
 
-  // construct the url
-  var apiUrl = 'https://maps.googleapis.com/maps/api/js'
-  var params = Object.keys(options).map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(options[key])}`)
-  var url = `${apiUrl}?${params.join('&')}`
+    // construct the url
+    var apiUrl = 'https://maps.googleapis.com/maps/api/js';
+    var params = Object.keys(options).map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(options[key])}`);
+    var url = `${apiUrl}?${params.join('&')}`;
 
-  // create and append the script to body
-  var script = document.createElement('script')
-  script.src = url
-  script.async = true
-  script.defer = true
-  document.body.appendChild(script)
-
-  app.component('location-picker', LocationPicker)
+    // create and append the script to body
+    var script = document.createElement('script');
+    script.src = url;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
 }
+export default LocationPicker;
+
